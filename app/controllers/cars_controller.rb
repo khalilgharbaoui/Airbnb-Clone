2e1df2ec5df96e01a0b841e68534c594debd259e
Miloucodaisseur <post.milou@gmail.com>
2016-02-09 17:07:38 +0100

    migrated car file

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,26 @@
+class CarsController < ApplicationController
+  # A list of all available cars
+  def index
+    @cars = Car.all
+  end
+
+# Add a new car as a provider
+  def new
+      @car = Car.new
+  end
+end
+
+# Create a car from the form params
+  def create
+    if @car = Car.create(car_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+
+def car_params
+  params_require(:car).permit(:model, :description, :image)
+end
