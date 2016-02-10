29df3d4a1ea14fe0e2ef1199d4a0e3fdb70c62ee
MaurcioGarc1a <maoricio07@gmail.com>
2016-02-10 11:59:51 +0100

    consumer provider pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,14 @@ Rails.application.routes.draw do
   resources :users
 
 
-
-
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  get '/provider/index' =>	'provider#index', as: :provider_index
+  get '/consumer/index' =>	'consumer#index', as: :consumer_index
   # Example resource route with options:
   #   resources :products do
   #     member do
