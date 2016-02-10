2763e7490980d97985ba2f4f1ec7d9bed0073a04
MaurcioGarc1a <maoricio07@gmail.com>
2016-02-10 12:55:19 +0100

    p/c controllers

---
+++ app/controllers/consumers_controller.rb
@@ -0,0 +1,25 @@
+class ConsumersController < ApplicationController
+  # A list of all available providers
+  def index
+    @consumers = Consumer.all
+  end
+
+  def new
+    @consumer = Consumer.new
+  end
+
+  # Create a provider from the form params
+  def create
+    if @consumer = Consumer.create(consumer_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def consumer_params
+    params_require(:name).permit(:last_name, :email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,17 +10,12 @@ Rails.application.routes.draw do
   resources :provider
   resources :consumer
 
-  get 'consumer/:format' => 'consumer#index'
-  get 'provider/:format' => 'provider#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
-
   # Example resource route with options:
   #   resources :products do
   #     member do
