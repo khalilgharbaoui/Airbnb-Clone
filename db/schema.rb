2afc050f12329bf4ac348ab797c3ad0c412443a5
khalilgharbaoui <kaygeee@gmail.com>
2016-02-11 16:54:44 +0100

    Fixed merging problem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211093817) do
+ActiveRecord::Schema.define(version: 20160211134930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,14 +19,14 @@ ActiveRecord::Schema.define(version: 20160211093817) do
   create_table "cars", force: :cascade do |t|
     t.string   "model"
     t.string   "image"
-    t.integer  "owner_id"
     t.text     "description"
+    t.string   "adress"
+    t.integer  "price"
+    t.text     "begin_date"
+    t.text     "end_date"
+    t.integer  "provider_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "begin_date"
-    t.string   "end_date"
-    t.integer  "price"
-    t.string   "location"
   end
 
   create_table "consumers", force: :cascade do |t|
@@ -37,6 +37,22 @@ ActiveRecord::Schema.define(version: 20160211093817) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "installs", force: :cascade do |t|
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "installs", ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true, using: :btree
+
   create_table "providers", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -46,12 +62,21 @@ ActiveRecord::Schema.define(version: 20160211093817) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "model"
-    t.integer  "owner_id"
-    t.integer  "client_id"
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
   end
+
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
